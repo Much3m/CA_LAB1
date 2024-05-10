c3dfb3ed58dc55d6c1752719f77ea50997de98ed
Much3m <kga1800@uos.ac.kr>
2024-05-10 11:08:56 +0900

    Decoded Added

--- 1.c
+++ 1.c
@@ -34,15 +34,22 @@ enum OPCODE {
 // beq  1100011(99), 000, n.a           SB-Type     
 // blt  1100011(99), 100, n.a           SB-Type
 
+void print_binary(unsigned int n, int count) {
+	if (count != 0)
+		print_binary(n/2,count -1);
+	printf("%u",n%2);
+}
+
 
 void parse_instructions(void) {
-    int temp = 0;
-    int running = 1;
-    while (running) {
-        int instruction = inst_mem[temp];
-        printf("%u\n",instruction);
+	int temp = 0;
+	int running = 1;
+	while (running) {
+		int instruction = inst_mem[temp];
+		print_binary(instruction,31);
+		printf("\n");
 		int opcode = (((instruction) & 0x7F) + ((instruction >> 12) & 0x7)); 
-        int rs1,    // source reg.1
+		int rs1,    // source reg.1
 			rs2,    // source reg.2
 			rd,     // destination reg.
 			imm,    // immediate
@@ -53,28 +60,28 @@ void parse_instructions(void) {
 				rd = (instruction >> 7) & 0x1F;
 				rs1 = (instruction >> 15) & 0x1F;
 				rs2 = (instruction >> 20) & 0x1F;
-                printf("ADD Instruction : 00000000011000101000001110110011\nrd = %d, rs1 = %d, rs2 = %d\n",rd,rs1,rs2);
+				printf("ADD Instruction\nrd = %d, rs1 = %d, rs2 = %d\n",rd,rs1,rs2);
 				temp++;
 				break;
 			case ADDI:
 				rd = (instruction >> 7) & 0x1F;
 				rs1 = (instruction >> 15) & 0x1F;
 				imm = (instruction >> 20) & 0xFFF;
-                printf("ADDI Type Instruction\n rd = %d, rs1 = %d, imm = %d\n",rd,rs1,imm);
+				printf("ADDI Type Instruction\n rd = %d, rs1 = %d, imm = %d\n",rd,rs1,imm);
 				temp++;
 				break;
 			case LW:
 				rd = (instruction >> 7) & 0x1F;
 				rs1 = (instruction >> 15) & 0x1F;
 				imm = (instruction >> 20) & 0xFFF;
-                printf("LW Type Instruction\n rd = %d, rs1 = %d, imm = %d\n",rd,rs1,imm);
+				printf("LW Type Instruction\n rd = %d, rs1 = %d, imm = %d\n",rd,rs1,imm);
 				temp++;
 				break;
 			case SW:
 				rs1 = (instruction >> 15) & 0x1F;
 				rs2 = (instruction >> 20) & 0x1F;
-                imm = ((instruction >> 7) & 0x1F) + (((instruction >> 25) & 0x7F) << 5);
-                printf("SW Type Instruction\n rs1 = %d, imm = %d\n",rs1,imm);
+				imm = ((instruction >> 7) & 0x1F) + (((instruction >> 25) & 0x7F) << 5);
+				printf("SW Type Instruction\n rs1 = %d, imm = %d\n",rs1,imm);
 				temp++;
 				break;
 			case BEQ:
@@ -82,15 +89,15 @@ void parse_instructions(void) {
 				 (((instruction >> 7) & 0x1) << 10) + (((instruction >> 31) & 0x1) << 11); 
 				rs1 = (instruction >> 15) & 0x1F;
 				rs2 = (instruction >> 20) & 0x1F;
-                printf("BEQ Type Instruction\n rs1 = %d, rs2 = %d, imm = %d \n",rs1,rs2,imm);
+				printf("BEQ Type Instruction\n rs1 = %d, rs2 = %d, imm = %d \n",rs1,rs2,imm);
 				temp++;
 				break;
-            case BLT:
+			case BLT:
 				imm = ((instruction >> 8) & 0xF) + (((instruction >> 25) & 0x3F) << 4) +\
 				 (((instruction >> 7) & 0x1) << 10) + (((instruction >> 31) & 0x1) << 11); 
 				rs1 = (instruction >> 15) & 0x1F;
 				rs2 = (instruction >> 20) & 0x1F;
-                printf("BLT Type Instruction\n rs1 = %d, rs2 = %d, imm = %d \n",rs1,rs2,imm);
+				printf("BLT Type Instruction\n rs1 = %d, rs2 = %d, imm = %d \n",rs1,rs2,imm);
 				temp++;
 				break;
 			case HALT:
@@ -102,8 +109,8 @@ void parse_instructions(void) {
 				running = 0;
 				break;
 		}
-        printf("\n\n");
-    }
+		printf("\n\n");
+	}
 
 
 
@@ -158,64 +165,66 @@ int main() {
 	registers[7] = 30;
 
 	////////               수정금지 영역 끝             ///////////
-    // parse_instructions();
+	// parse_instructions();
 
 	// flag for end-of-program
 	int running = 1;
+	my_print_register(); printf("\n\n");
 
 	while (running) {
 		//Fetch 
 		int instruction = inst_mem[pc];
 		int opcode = (((instruction) & 0x7F) + ((instruction >> 12) & 0x7)); 
 
-        printf("OP CODE : %d = %d + %d\n", opcode,((instruction) & 0x7F),((instruction >> 12) & 0x7));
-        printf("PC : %d\n",pc);
+		// printf("OP CODE : %d = %d + %d\n", opcode,((instruction) & 0x7F),((instruction >> 12) & 0x7));
+		printf("\033[36mPC : %d\n",pc);
 		// int funct3 = (instruction >> 12) & 0x7; //TODO
 		int rs1,    // source reg.1
 			rs2,    // source reg.2
 			rd,     // destination reg.
 			imm,    // immediate
 			addr;   // base address
-
+		
 		switch (opcode) {
 			case ADD:
 				rd = (instruction >> 7) & 0x1F;
 				rs1 = (instruction >> 15) & 0x1F;
 				rs2 = (instruction >> 20) & 0x1F;
-                printf("ADD Type Instruction\n rd = %d, rs1 = %d, rs2 = %d\n",rd,rs1,rs2);
-                printf("Before ADDI Type Instruction -> rd[%d] , rs1[%d] , imm[%d]\n",registers[rd],registers[rs1],registers[rs2]);
+				// printf("ADD Type Instruction\n rd = %d, rs1 = %d, rs2 = %d\n",rd,rs1,rs2);
+				printf("\033[32mDecoded Instruction : add x%02d, x%02d, x%02d\n\033[0m",rd, rs1,rs2);
+				printf("Before add Type Instruction\nx%02d = %d , x%02d = %d , x%02d = %d\n",rd,registers[rd],rs1,registers[rs1],rs2,registers[rs2]);
 				registers[rd] = registers[rs1] + registers[rs2];
 				pc++;
-                printf("After : ADD Type Result -> rd[%d] = rs1[%d] + rs2[%d]\n",registers[rd],registers[rs1],registers[rs2]);
+				printf("After add Type Instruction\nx%02d = %d\n",rd,registers[rd]);
 				break;
 			case ADDI:
 				rd = (instruction >> 7) & 0x1F;
 				rs1 = (instruction >> 15) & 0x1F;
 				imm = (instruction >> 20) & 0xFFF;
-                printf("ADDI Type Instruction\n rd = %d, rs1 = %d, imm = %d\n",rd,rs1,imm);
-                printf("Before ADDI Type Instruction -> rd[%d] , rs1[%d] , imm[%d]\n",registers[rd],registers[rs1],imm);
+				printf("\033[32mDecoded Instruction : addi x%02d, x%02d, %d\n\033[0m",rd, rs1,imm);
+				printf("Before addi Type Instruction\nx%02d = %d , x%02d = %d\n",rd,registers[rd],rs1,registers[rs1]);
 				registers[rd] = registers[rs1] + imm;
 				pc++;
-                printf("After : ADDI Type Result -> rd[%d] = rs1[%d] + imm[%d]\n",registers[rd],registers[rs1],imm);
+				printf("After addi Type Instruction\nx%02d = %d\n",rd,registers[rd]);
 				break;
 			case LW:
 				rd = (instruction >> 7) & 0x1F;
 				rs1 = (instruction >> 15) & 0x1F;
 				imm = (instruction >> 20) & 0xFFF;
-                printf("LW Type Instruction\n rd = %d, rs1 = %d, imm = %d\n",rd,rs1,imm);
-                printf("Before LW Type Instruction -> rd[%d], data_mem[%d]\n",registers[rd], data_mem[registers[rs1] + imm/4]);
+				printf("\033[32mDecoded Instruction : lw x%02d ,%d(x%02d)\n\033[0m",rd,imm,rs1);
+				printf("Before LW Type Instruction\n x%02d = %d, data_mem[%d] = %d\n",rd, registers[rd], registers[rs1] + imm/4, data_mem[registers[rs1] + imm/4]);
 				registers[rd] = data_mem[registers[rs1] + imm/4];
-                printf("After LW Type Result -> rd = %d\n",registers[rd]);
+				printf("After LW Type Result\nx%02d = %d\n",rd, registers[rd]);
 				pc++;
 				break;
 			case SW:
 				rs1 = (instruction >> 15) & 0x1F;
 				rs2 = (instruction >> 20) & 0x1F;
-                imm = ((instruction >> 7) & 0x1F) + (((instruction >> 25) & 0x7F) << 5);
-                printf("SW Type Instruction\n, rs1 = %d, imm = %d\n",rs1,imm);
-                printf("Before SW Type Instruction -> rd[%d], data_mem[%d]\n",registers[rd], data_mem[registers[rs1] + imm/4]);
+				imm = ((instruction >> 7) & 0x1F) + (((instruction >> 25) & 0x7F) << 5);
+				printf("\033[32mDecoded Instruction : sw x%02d ,%d(x%02d)\n\033[0m",rs2,imm,rs1);
+				printf("Before SW Type Instruction\n x%02d = %d, data_mem[%d] = %d\n",rs2, registers[rs2], registers[rs1] + imm/4, data_mem[registers[rs1] + imm/4]);
 				data_mem[registers[rs1] + imm/4] = registers[rs2];
-                printf("After SW Type Result -> Data = %d\n",data_mem[registers[rs1] + imm/4]);
+				printf("After SW Type Result\ndata_mem[%d] = %d\n",registers[rs1] + imm/4, data_mem[registers[rs1] + imm/4]);
 				pc++;
 				break;
 			case BEQ:
@@ -223,23 +232,24 @@ int main() {
 				 (((instruction >> 7) & 0x1) << 10) + (((instruction >> 31) & 0x1) << 11); 
 				rs1 = (instruction >> 15) & 0x1F;
 				rs2 = (instruction >> 20) & 0x1F;
-                printf("BLT Type Instruction\n, rs1 = %d, rs2 = %d, imm = %d \n",rs1,rs2,imm);
+				printf("\033[32mDecoded Instruction : beq x%02d , x%02d, %d\n\033[0m",rs1,rs2,(imm<<1)/4);
 				if(registers[rs1] == registers[rs2]) { pc += (imm<<1)/4;}
-                else pc++;
-                // pc++;
+				else pc++;
+				// pc++;
 				break;
-            case BLT:
+			case BLT:
 				imm = ((instruction >> 8) & 0xF) + (((instruction >> 25) & 0x3F) << 4) +\
 				 (((instruction >> 7) & 0x1) << 10) + (((instruction >> 31) & 0x1) << 11); 
 				rs1 = (instruction >> 15) & 0x1F;
 				rs2 = (instruction >> 20) & 0x1F;
-                printf("BLT Type Instruction\n, rs1 = %d, rs2 = %d, imm = %d \n",rs1,rs2,imm);
+				printf("\033[32mDecoded Instruction : blt x%02d , x%02d, %d\n\033[0m",rs1,rs2,(imm<<1)/4);
 				if(registers[rs1] < registers[rs2]) { pc += (imm<<1)/4;}
-                else pc++;
-                // pc++;
+				else pc++;
+				// pc++;
 				break;
 			case HALT:
 				running = 0;
+				printf("\033[32mDecoded Instruction : Halt\n\033[0m");
 				printf(" ** END OF THE PROGRAM ** \n");
 				break;
 			default:
@@ -247,9 +257,9 @@ int main() {
 				running = 0;
 				break;
 		}
-        printf("\n\n");
-        my_print_register();
-        printf("\n\n");
+		printf("\n\n");
+		my_print_register();
+		printf("\n\n");
 	}
 
 	// Print final register values
@@ -266,28 +276,26 @@ void print_register_values(){
 }
 
 void my_print_register() { 
-    printf("Current Register Values \n");
-    // int k = 0;
-    // for (int i = 0; i < 4 ; i++) {
-    //     for (int j = 0; j < 8; j++) {
-    //         if (registers[k] != 0)
-    //             printf("| x%02d : %02d ", k, registers[k]);
-    //         else
-    //             printf("| x%02d : %02d ", k, registers[k]);
-    //         k++;
-    //     }
-    //     printf("|\n");
-    // }
-
-
-    int i = 0;
-    while (i < 32) {
-        if (registers[i] != 0)
-            printf("| \033[31mx%02d : %02d \033[0m", i, registers[i]);
-        else
-            printf("| x%02d : %02d ", i, registers[i]);
-        i++;
-        if (i % 8 == 0 && i != 0)
-            printf("\n");
-    }
+	printf("Current Register Values \n");
+	int i = 0;
+	while (i < 32) {
+		if (registers[i] != 0)
+			printf("| \033[31mx%02d : %02d \033[0m", i, registers[i]);
+		else
+			printf("| x%02d : %02d ", i, registers[i]);
+		i++;
+		if (i % 8 == 0 && i != 0)
+			printf("\n");
+	}
+	printf("Current Data_Memory Values \n");
+	i = 0;
+	while (i < 40) {
+		if (data_mem[i] != 0)
+			printf("| \033[31mMEM%02d : %02d \033[0m", i, data_mem[i]);
+		else
+			printf("| MEM%02d : %02d ", i, data_mem[i]);
+		i++;
+		if (i % 8 == 0 && i != 0)
+			printf("\n");
+	}
 }
